5ee0db2a688eafd388b7435f02110eaee3b9b9ee
George Staniland <georgestaniland@gmail.com>
2023-08-09 14:24:17 +1200

    3d

---
+++ components/AnimatedText.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+    // This reference will give us direct access to the mesh
+    const meshRef = useRef()
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+    // Return view, these are regular three.js elements expressed in JSX
+
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1.5 : 1}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxGeometry args={[4, 4, 4]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
+
+function CustomPointLight() {
+    const light = useRef()
+
+    return (
+        <pointLight position={[5, 3, 10]} intensity={1} />
+    )
+}
+
+export default function AnimatedText() {
+
+    return (
+        <div className="a-test-class">
+            <Canvas>
+                <CustomPointLight />
+                <Box />
+            </Canvas>
+        </div>
+    )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
+import AnimatedText from '../../components/AnimatedText'
+import Test from '../../components/Test'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main >
-      <h1>George Staniland - Frontend Developer</h1>
-      <div>
-        <p>Hi, Greetings. I'm a developer with expertise in web technologies and a specific focus on frontend development. I like seeing my dog, and going on adventures.</p>
+      <h1 className={styles.pb}>
+        George Staniland
+      </h1>
+      <div className={`${styles.text_container} ${styles.pb}`}>
+        <p className={styles.body_text}>Hello &#128075; Im a frontend web developer. <br /> Im inspired when making beautiful, intuitive experiences with code. <br /> Outside of this work, I like photography, seeing my dog, and travelling. </p>
       </div>
-      <h2>Recent Projects</h2>
+      <h2 className={styles.pb_sml} >Recent Projects</h2>
       <ul>
+        <li><a href="https://www.georgestaniland.net/" target="_blank">Photography Portfolio (My own work)</a></li>
         <li><a href="https://www.lomocean.com/" target="_blank">LOMOcean Marine</a></li>
-        <li>Capital Scaffolding</li>
-        <li>My own photography portfolio</li>
-        <li>Innovative Concepts</li>
+        <li><a href="https://capitalscaffolding.co.nz/" target="_blank">Capital Scaffolding</a></li>
       </ul>
-    </main>
+      <AnimatedText />
+    </main >
   )
 }
