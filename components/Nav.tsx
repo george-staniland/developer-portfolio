719921bd6b08c4ed35d8f8303817581ee48e6b26
George Staniland <georgestaniland@gmail.com>
2025-11-20 17:35:52 +1300

    info popup working

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,43 +6,32 @@ import { overlay } from "three/examples/jsm/nodes/Nodes.js";
 function Nav() {
     const [isActive, setIsActive] = useState(false);
 
-    const btnStyle = useSpring({
-        height: isActive ? '540px' : '28px',
-        width: isActive ? '540px' : '28px',
-    })
-
-    // Spring for overlay content opacity
-    const overlayStyle = useSpring({
-        opacity: isActive ? 1 : 0,
-        delay: isActive ? 990 : 0, // Delay opacity change until button has expanded
-    });
 
     return (
         <nav className="main-nav" >
             <div className="inner px">
                 <h2>George Staniland</h2>
-                <animated.div
+                <div
                     role="button"
                     className={`menu btn nav-btn ${isActive ? 'is-active' : 'non-active'}`}
                     onClick={() => setIsActive(!isActive)}
-                    style={btnStyle}
                 >
                     <div className="open-info__icon">
                         <span className="horizontal-line line"></span>
                         <span className="vertical-line line"></span>
                     </div>
-                    <animated.section style={overlayStyle} className="overlay-content">
-                        <div className="overlay-inner">
+                    <section className="overlay-content">
+                        <div className="overlay-inner px py">
                             <p>Hello ✋</p>
                             <div className="items">
-                                <p>I'm currently in Auckland, New Zealand</p>
+                                <p>I'm currently in Wellington, New Zealand</p>
                                 <p className="email" >email:
                                     <object className="email" width="292" height="27" data="/contact-e.svg" type="image/svg+xml"></object> </p>
                                 <p className="phone" >whatsapp:  <object className="phone" width="171" height="20" data="/contact-p.svg" type="image/svg+xml"></object></p>
                             </div>
                         </div>
-                    </animated.section>
-                </animated.div>
+                    </section>
+                </div>
             </div>
         </nav>
     )
