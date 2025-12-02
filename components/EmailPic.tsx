5abd06676fb65e362f9b5770e2b06e3cf878ab54
George Staniland <georgestaniland@gmail.com>
2025-12-02 13:04:08 +1300

    design fixes

---
+++ components/EmailPic.tsx
@@ -0,0 +1,14 @@
+export default function EmailPic () {
+    return (
+        <article className="email-wrap ">
+            <p>Email:</p>
+         <object 
+            data="/e-pic.svg" 
+            type="image/svg+xml"
+            width="242"
+            height="20"
+        >
+        </object>
+        </article>
+    )
+}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,30 @@
 "use client"
 import { useState } from "react"
-import { useSpring, animated, config } from '@react-spring/web'
-import { overlay } from "three/examples/jsm/nodes/Nodes.js";
+import EmailPic from "./EmailPic";
+import PhonePic from "./PhonePic";
+import { GiConfirmed } from "react-icons/gi";
 
 function Nav() {
     const [isActive, setIsActive] = useState(false);
 
+    const [copied, setCopied] = useState<{ email: boolean; phone: boolean }>({
+        email: false,
+        phone: false
+    });
 
 
-
-    async function handleCopy(text: string) {
+    async function handleCopy(key: "email" | "phone", text: string) {
         try {
             const clipboardItem = new ClipboardItem({ "text/plain": text });
             await navigator.clipboard.write([clipboardItem]);
+
+           
+            setCopied(prev => ({ ...prev, [key]: true }));
+
+          
+            setTimeout(() => {
+                setCopied(prev => ({ ...prev, [key]: false }));
+            }, 2000);
         } catch (err) {
             console.log('error copying')
         }
@@ -34,28 +46,34 @@ function Nav() {
                     </div>
                     <section className="overlay-content">
                         <div className="overlay-inner px py ">
-                            <p className="fb">Hello ✋</p>
+                            <p className="fb-small">Hello ✋</p>
                             <div className="items">
-                                <p className="fb">I'm currently in Wellington, New Zealand.</p>
-                                <p className="email fb" >email:
-                                    <object className="email" width="270" height="24" data="/contact-e.svg" type="image/svg+xml"></object> </p>
-                                <p className="phone fb" >whatsapp:  <object className="phone" width="150" height="17" data="/contact-p.svg" type="image/svg+xml"></object></p>
+                                <p className="fb-small">I'm currently in Wellington, New Zealand.</p>
+                                <p className="fb-small">For project enquires please reach out below.</p>
+                                <div className="svgs-wrap">
+                                    <EmailPic/>
+                                    <PhonePic/>  
+                                </div>
                             </div>
                             <div className="btns">
 
-                                 <button onClick={(e) => { 
+                                 <button 
+                                 onClick={(e) => { 
                                     e.stopPropagation();
-                                    handleCopy("georgestaniland@gmail.com");
-                                }}>
+                                    handleCopy("email","georgestaniland@gmail.com");
+                                }}
+                                >
                                     Copy Email
+                                    { copied.email && <GiConfirmed />}
                                 </button>
 
                             
                                 <button onClick={(e) => { 
                                     e.stopPropagation();
-                                    handleCopy("0225109709");
+                                    handleCopy("phone","0225109709");
                                 }}>
                                     Copy WhatsApp
+                                    { copied.phone && <GiConfirmed />}
                                 </button>
 
                             </div>
