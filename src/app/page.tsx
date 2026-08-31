f91ac1068a0323cec6def17e3377c5b8d70db900
George Staniland <georgestaniland@gmail.com>
2026-08-31 13:51:33 +0700

    remove old components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Nav from '@/components/Nav'
-import HeroSectionWrap from '@/components/HeroSectionWrap'
 import MainText from '@/components/MainText'
-import ProjectsGrid from '@/components/ProjectsGrid'
 import { executeQuery } from '@datocms/cda-client';
 import { GET_PROJECTS } from '@/lib/queries/queries';
 
 export default async function Home() {
 
-  const projects = await executeQuery(GET_PROJECTS, {
-    token: process.env.DATOCMS_API_TOKEN,
-  });
+  // const projects = await executeQuery(GET_PROJECTS, {
+  //   token: process.env.DATOCMS_API_TOKEN,
+  // });
 
   return (
     <>
